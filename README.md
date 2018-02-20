8be922ed3f9f835f9a4e546f05f4885b91dc7335
Joshua Higgins <xxhiggins723xx@gmail.com>
2018-02-19 20:09:12 -0700

    first push

---
+++ README.md
@@ -0,0 +1,1 @@
+CS313 Team 07 Activity
---
+++ web/profile.php
@@ -0,0 +1,25 @@
+<?php
+    include("includes/header.php");
+    include("login.php");
+    include("manageItems.php");
+
+    // if not logged in redirect to index
+    if(!isset($_SESSION["userId"])) {
+        header('Location: index.php');
+    }
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <h2>Hello <span style="font-style: italic;"><?php echo $_SESSION["user"]; ?></span></h2>
+            <span><?php echo $login_session; ?></span>
+            <a href="logout.php">Log Out</a>
+            <hr/>
+        </div>
+    </div>
+</div>
+
+<?php
+    include("includes/footer.php");
+?>
